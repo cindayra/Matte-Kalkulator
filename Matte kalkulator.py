66a962760d2bcb7e16effec5d69d47deba38179f
cindayra <cindayra@gmail.com>
2017-04-05 15:49:45 +0200

    Ein liten fiks og la til sannsyns-rekning
    Eg har fiksa slik at du kan bruke akuratt så mange stor bokstavar du vil

--- Matte kalkulator.py
+++ Matte kalkulator.py
@@ -1,11 +1,12 @@
 import math
-import string
 
 print ('Her er lista med forskjellige kalkulatorar, skriv namnet på kalkulatoren du vil bruke.')
 print ('Pythagoras')
 print ('Forhold')
+print ('Sannsyn')
+
 svar = input()
-str.lower(svar)
+svar = str.lower(svar)
 if svar == 'pythagoras':
     svar = input('Vil du finne hypotenus eller ein katet? ')
     str.lower(svar)
@@ -41,6 +42,7 @@ if svar == 'pythagoras':
         h = math.sqrt(h)
 
         print (h)
+        
 elif svar == 'forhold':
 
     a = input ('Skriv inn lengda på den eine kjende linja på den "ukjende" trekanten ')
@@ -56,5 +58,19 @@ elif svar == 'forhold':
     svar = svar / B
 
     print (svar)
+
+elif svar == 'sannsyn':
+    Gunstige = input ('Skriv inn antall gunstige utfall ')
+    Moglege = input ('Skriv inn antall moglege utfall ')
+
+    Gunstige = int (float (Gunstige))
+    Moglege = int (float (Moglege))
+
+    svar = Gunstige / Moglege
+
+    svar = str (svar)
+
+    print ('Sannsynet for eit gunstigt utfall er ' + svar)
+
 else:
     print ('Vennligst skriv eit av alternativa')
