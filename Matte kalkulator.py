a4cbad9f082a7cbab6b56213a1eda4ec29c508ed
cindayra <cindayra@gmail.com>
2017-04-18 20:07:37 +0200

    La til ny funksjon og liten feilretting i skrivinga
    La til funskjonen til å finne hypotenus til ein pyramide og endra
    teksten til forhold.

--- Matte kalkulator.py
+++ Matte kalkulator.py
@@ -11,21 +11,45 @@ if svar == 'pythagoras':
     svar = input('Vil du finne hypotenus eller ein katet? ')
     str.lower(svar)
     if svar == 'hypotenus':
+        svar = input ('Av ein pyramide eller ein trekant?')
+        str.lower (svar)
+
+        if svar == 'trekant':
+            
+            k1 = input ('Katet ')
+            k2 = input ('Katet ')
+
+            k1 = int(float (k1))
+            k2 = int(float (k2))
+
+            k1 = k1 ** 2
+            k2 = k2 ** 2
+
+            h = k1 + k2
+
+            h = math.sqrt(h)
+
+            print (h)
+            
+        elif svar == 'pyramide':
+            
+            k1 = input ('Lenga av ein side på botnplata ')
+            k2 = input ('Høga på pyramiden ')
+
+            k1 = int (float (k1))
+            k2 = int (float (k2))
+
+            k1 = k1 / 2
+
+            k1 = k1 ** 2
+            k2 = k2 ** 2
+
+            h = k1 + k2
+
+            h = math.sqrt (h)
+            
+            print (h)
         
-        k1 = input ('Katet ')
-        k2 = input ('Katet ')
-
-        k1 = int(k1)
-        k2 = int(k2)
-
-        k1 = k1 ** 2
-        k2 = k2 ** 2
-
-        h = k1 + k2
-
-        h = math.sqrt(h)
-
-        print (h)
     elif svar == 'katet':
         
         k1 = input ('Hypotenus ')
@@ -45,10 +69,10 @@ if svar == 'pythagoras':
         
 elif svar == 'forhold':
 
-    a = input ('Skriv inn lengda på den eine kjende linja på den "ukjende" trekanten ')
+    a = input ('1) Skriv inn lengda på den eine kjende linja på den "ukjende" trekanten ')
 
-    A = input ('Skriv inn lengda på linja som er lik X på motsatt trekant ')
-    B = input ('Skriv inn lengda på linja på den "kjende" trekanten som er parallell til den du skreiv til den "ukjende" ')
+    A = input ('2) Skriv inn lengda på linja som er lik X på motsatt trekant ')
+    B = input ('3) Skriv inn lengda på linja på den "kjende" trekanten som er parallell til den du skreiv til 1) ')
 
     a = int (float(a))
     A = int (float(A))
